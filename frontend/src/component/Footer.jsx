377ba1b36d06a7da449b35bd0feb45502308c430
Samind2 <suphaneerangsirat@gmail.com>
2024-09-29 07:52:48 +0700

    succress

--- frontend/src/component/Footer.jsx
+++ frontend/src/component/Footer.jsx
@@ -1,4 +1,4 @@
-
+//ส่วนท้ายของหน้าเว็บซึ่งจะใส่ข้อมูลต่างๆและลิขสิทธิ์ต่างๆ
 const Footer = () => {
   return (
     <footer className="footer footer-center bg-base-200 text-base-content rounded p-10">
